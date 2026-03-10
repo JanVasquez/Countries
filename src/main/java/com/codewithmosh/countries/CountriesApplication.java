a107f83bd7cd925294729e08d88bbe5c84c0534e
jancervantesvasquez <janorlando3003@hotmail.com>
2026-03-10 15:33:44 +0100

    BetterCountries push

---
+++ src/main/java/com/codewithmosh/countries/CountriesApplication.java
@@ -0,0 +1,13 @@
+package com.codewithmosh.countries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class CountriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountriesApplication.class, args);
+    }
+
+}
